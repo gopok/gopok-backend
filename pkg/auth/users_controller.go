9e06b780e264313510a19e1a090f17f337725dfb
alufers <alufers@wp.pl>
2018-04-05 22:18:49 +0200

    Added post user route

--- pkg/auth/users_controller.go
+++ pkg/auth/users_controller.go
@@ -1,16 +1,34 @@
 package auth
 
-import "github.com/gopok/gopok-backend/pkg/core"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/gopok/gopok-backend/pkg/core"
+)
 
 /*
 UsersController handles actions with user administration.
 */
-type UsersController struct{}
+type UsersController struct {
+	app         *core.Application
+	usersRouter *mux.Router
+}
 
 /*
 Register registers the controller
 */
-func (*UsersController) Register(app *core.Application) {
+func (uc *UsersController) Register(app *core.Application) {
+	uc.app = app
+	uc.usersRouter = app.Router.PathPrefix("/api/auth/users").Subrouter()
+	uc.usersRouter.HandleFunc("", uc.postUser).Methods("POST")
+	
+}
+
+func (uc *UsersController) postUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("TEST"))
 }
 
 func init() {
