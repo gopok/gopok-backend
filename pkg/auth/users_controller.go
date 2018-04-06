3e7ef9ab06c01e4ec5ec626cab1a485d90abf453
alufers <alufers@wp.pl>
2018-04-06 14:39:04 +0200

    Added users controller & postUser route

--- pkg/auth/users_controller.go
+++ pkg/auth/users_controller.go
@@ -1,9 +1,6 @@
 package auth
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gorilla/mux"
 
 	"github.com/gopok/gopok-backend/pkg/core"
@@ -23,12 +20,22 @@ Register registers the controller
 func (uc *UsersController) Register(app *core.Application) {
 	uc.app = app
 	uc.usersRouter = app.Router.PathPrefix("/api/auth/users").Subrouter()
-	uc.usersRouter.HandleFunc("", uc.postUser).Methods("POST")
-	
+	uc.usersRouter.HandleFunc("", core.WrapRest(uc.postUser)).Methods("POST")
+
 }
 
-func (uc *UsersController) postUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("TEST"))
+func (uc *UsersController) postUser(r *core.RestRequest) interface{} {
+	var u user
+	r.DecodeJSON(&u)
+	var allData map[string]string
+	r.DecodeJSON(&allData)
+	u.hashPassword(allData["password"])
+	err := uc.app.Db.C("users").Insert(u)
+	if err != nil {
+		r.SetCode(500)
+		return err.Error()
+	}
+	return u
 }
 
 func init() {
